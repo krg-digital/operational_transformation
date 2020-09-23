8024c436cf5c11e70443690148c86731eeaf3654
kevin <krg@tutamail.com>
2020-09-22 21:11:40 -0700

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 fn main() {
-    let stale_1 = "Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.";    
-    let latest_1 = "Repl.it uses operational transformations.";    
+    let stale_1 =
+        "Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.";
+    let latest_1 = "Repl.it uses operational transformations.";
     let ops_1 = vec![Operation::Skip(40), Operation::Delete(47)];
 
-    let stale_2 = "Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.";
-    let latest_2 = "We use operational transformations to keep everyone in a multiplayer repl in sync."; 
-    let ops_2 = vec![Operation::Delete(7), Operation::Insert(String::from("We")), Operation::Skip(4), Operation::Delete(1)];
+    let stale_2 =
+        "Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.";
+    let latest_2 =
+        "We use operational transformations to keep everyone in a multiplayer repl in sync.";
+    let ops_2 = vec![
+        Operation::Delete(7),
+        Operation::Insert(String::from("We")),
+        Operation::Skip(4),
+        Operation::Delete(1),
+    ];
 
-    
-    let test_1 = is_valid(
-        stale_1,
-        latest_1,
-        ops_1
-    );
+    let test_1 = is_valid(stale_1, latest_1, ops_1);
     println!("{}", test_1);
 
-    let test_2 = is_valid(
-        stale_2,
-        latest_2,
-        ops_2  
-    );
+    let test_2 = is_valid(stale_2, latest_2, ops_2);
     println!("{}", test_2);
 }
 
@@ -45,9 +44,13 @@ struct OperationalTransformation {
 
 impl OperationalTransformation {
     fn new(s: String, ops: Vec<Operation>) -> OperationalTransformation {
-        OperationalTransformation { s, position: 0, ops }    
+        OperationalTransformation {
+            s,
+            position: 0,
+            ops,
+        }
     }
-    
+
     fn transform(&mut self) -> String {
         let mut res = self.s.clone();
 
