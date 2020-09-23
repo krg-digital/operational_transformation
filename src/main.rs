c0d85e0c3c5a5aba5385b140e5935094f7e11ed7
kevin <krg@tutamail.com>
2020-09-22 20:36:39 -0700

    read and perform operations on a string

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+fn main() {
+    let stale_1 = "Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.";
+    let latest_1 = "We use operational transformations to keep everyone in a multiplayer repl in sync."; 
+    let ops_1 = vec![Operation::Delete(7), Operation::Insert(String::from("We")), Operation::Skip(4), Operation::Delete(1)];
+
+    
+    let test_1 = is_valid(
+        stale_1,
+        latest_1,
+        ops_1
+    );
+    println!("{}", test_1);
+}
+
+fn is_valid(stale: &str, latest: &str, otjson: Vec<Operation>) -> bool {
+    let mut ot = OperationalTransformation::new(String::from(stale), otjson);
+
+    ot.transform() == String::from(latest)
+}
+
+fn insert(s: String, ins: String) -> String {
+    let mut res = ins.clone();
+    res.push_str(&s);
+
+    res
+}
+
+#[derive(Debug)]
+struct OperationalTransformation {
+    s: String,
+    position: usize,
+    ops: Vec<Operation>,
+}
+
+impl OperationalTransformation {
+    fn new(s: String, ops: Vec<Operation>) -> OperationalTransformation {
+        OperationalTransformation { s, position: 0, ops }    
+    }
+    
+    fn transform(&mut self) -> String {
+        let mut res = self.s.clone();
+
+        for op in &self.ops {
+            match op {
+                Operation::Insert(ins) => res.insert_str(self.position, &ins),
+                Operation::Delete(n) => res.replace_range(self.position..(self.position + *n), ""),
+                Operation::Skip(n) => self.position += n,
+            }
+        }
+
+        res
+    }
+}
+
+#[derive(Debug)]
+enum Operation {
+    Insert(String),
+    Delete(usize),
+    Skip(usize),
+}
